af31cf36ba174105e55b6bbf4d7355484b679f77
arashik65 <mdashikur6398+@gmail.com>
2026-03-18 20:57:12 +0600

    premitive - undefined in js

---
+++ undefined.js
@@ -0,0 +1,24 @@
+//undefined
+let data;
+// console.log(data);
+const sum =(a,b)=>{
+    console.log(a,b);
+}
+sum(5);
+
+const student={
+    name:'Ashik',
+    age:22,
+    student:null
+}
+delete student.name;
+// console.log(student.marks);
+console.log(student.name);
+
+const arr = [1,2,3,4];
+delete arr[1];
+console.log(arr[1]);
+
+// console.log(typeof undefined);---> output- undefined hole undefined hobe 
+console.log(typeof null); //---> output - null hole object hobe 
+
