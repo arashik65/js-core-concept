88d953d12f2a0a60de9c8f72c2b21bfc7f615c8c
arashik65 <mdashikur6398+@gmail.com>
2026-03-23 17:13:19 +0600

    callback function in js

---
+++ callback.js
@@ -0,0 +1,19 @@
+// document.getElementById('#btn').addEventListener('click',function(){
+    
+// })
+
+// addEventListener('click',function(event){})
+
+    function settelife(name,isBCS,marriage,patri){
+        if(isBCS){
+           marriage(patri);
+        }
+
+    }
+
+    function bolokobul(patri){
+        console.log('kobul',patri)
+    }
+    settelife('Tomal',true,bolokobul,'nari');
+    // bolokobul('pori');
+
