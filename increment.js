feae37d521ab73a1b5ad7802d1049e9f5d1a2321
arashik65 <mdashikur6398+@gmail.com>
2026-03-24 17:50:29 +0600

    incremnet-decrement introduce in js

---
+++ increment.js
@@ -0,0 +1,13 @@
+//inclrement operation
+let a =5;
+a++; // a=6
+//post increment a++= 6
+// console.log(a++); 
+//pre increment
+console.log(++a); //++a= 7
+console.log(a); //a=7
+
+//decrement operation 
+let b= 10;
+b--; //b--=9
+console.log(--b); //--b=8
