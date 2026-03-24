c5bbde6d445f3c38d7e15aaf92185e6a5f6be861
arashik65 <mdashikur6398+@gmail.com>
2026-03-24 15:10:34 +0600

    filter in js

--- filter.js
+++ filter.js
@@ -1,3 +1,26 @@
 const numbers =[1,2,3,4,5];
 const evenNumbers = numbers.filter(num=>num%2==0);
-console.log(evenNumbers);
+console.log(evenNumbers); //[ 2, 4 ]
+
+const friends =['Ashik','naved','khaled','sajed','nahid','nasib'];
+const Nfriends = friends.filter(frd=> frd[0]==='n');
+console.log(Nfriends); //[ 'naved', 'nahid', 'nasib' ]
+
+
+const students =[
+    {name:'Ashik', age:22},
+    {name:'Zaved', age:31},
+    {name:'Naved', age:23},
+    {name:'Khaled', age:20},
+    {name:'Sajed', age:34},
+    {name:'Nahid', age:22},
+    {name:'Nasib', age:41},
+]
+const oldStudents= students.filter(std=>std.age>30);
+console.log(oldStudents);   
+
+//output-----> [
+//   { name: 'Zaved', age: 31 },
+//   { name: 'Sajed', age: 34 },
+//   { name: 'Nasib', age: 41 }
+// ]
